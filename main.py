afad9d596d43bba9faf9d7777005c9944ad2a734
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2022-02-13 22:45:10 +0300

    Done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US map")
@@ -6,17 +7,34 @@ screen.title("US map")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.penup()
+data_states = pandas.read_csv("50_states.csv")
+all_states = data_states.state.to_list()
+guessed_states = []
+not_guessed = []
+while len(guessed_states) < 50:
+    user_input = turtle.textinput(f"Enter state", f"{len(guessed_states)}/50 Guess another state?").title()
 
+    if user_input == 'Exit' or user_input is None:
+        break
+    if user_input in all_states:
+        guessed_states.append(user_input)
+        row_data = data_states[data_states.state == user_input]
+        pen.goto(int(row_data.x), int(row_data.y))
+        pen.write(user_input)
+for item in all_states:
+    if item not in guessed_states:
+        not_guessed.append(item)
 
-user_input = turtle.textinput("Enter state", "Geuss another state")
-
-
-
-def on_click_on_screen(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(on_click_on_screen)
-
-turtle.mainloop()
-screen.exitonclick()
+print(not_guessed)
+final_list = pandas.DataFrame(not_guessed)
+final_list.to_csv('not_guessed_file.csv')
+# def on_click_on_screen(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(on_click_on_screen)
+# turtle.mainloop()
+# screen.exitonclick()
