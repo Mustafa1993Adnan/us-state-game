416437e8f8d631098afa7c71aaadd9fde3e3243e
Mustafa Adnan <Mustafa1993tofi@gmail.com>
2022-02-18 19:19:38 +0300

    Tutorial Done

--- main.py
+++ main.py
@@ -18,16 +18,23 @@ while len(guessed_states) < 50:
     user_input = turtle.textinput(f"Enter state", f"{len(guessed_states)}/50 Guess another state?").title()
 
     if user_input == 'Exit' or user_input is None:
+        print(guessed_states)
+        not_guessed = [notGuest for notGuest in all_states if notGuest not in guessed_states]
+        print(not_guessed)
+        final_list = pandas.DataFrame(not_guessed)
+        final_list.to_csv('not_guessed_file.csv')
         break
     if user_input in all_states:
         guessed_states.append(user_input)
         row_data = data_states[data_states.state == user_input]
         pen.goto(int(row_data.x), int(row_data.y))
         pen.write(user_input)
-for item in all_states:
-    if item not in guessed_states:
-        not_guessed.append(item)
+    # if user_input in all_states:
+    #     guessed_states.append(user_input)
+    #     row_data = data_states[data_states.state == user_input]
+    #     pen.goto(int(row_data.x), int(row_data.y))
+    #     pen.write(user_input)
+# for item in all_states:
+#     if item not in guessed_states:
+#         not_guessed.append(item)
 
-print(not_guessed)
-final_list = pandas.DataFrame(not_guessed)
-final_list.to_csv('not_guessed_file.csv')
